d877915d61a475c386095a373dd856c7e7a313a3
alecerioli <51276076+alecerioli@users.noreply.github.com>
2019-06-19 19:51:53 -0300

    Update registrador8.vhd

--- registrador8.vhd
+++ registrador8.vhd
@@ -4,8 +4,7 @@ use ieee.std_logic_1164.all;
 entity registrador8 is
 port(
 		Ip  : in std_logic_vector(7 downto 0);
-		selCP   : in std_logic ;
-		Ii  : in std_logic_vector(7 downto 0);
+		Load   : in std_logic ;
 		clk : in std_logic;
 		Qs	 : out std_logic_vector(7 downto 0)
 	);
@@ -19,15 +18,16 @@ port(
 		Q   : out std_logic_vector(7 downto 0)
 	);
 end component;
-	signal Ie : std_logic_vector(7 downto 0);
+	signal Ie,Qaux : std_logic_vector(7 downto 0);
 begin
-	Ie(0) <= (selCP and Ip(0)) or (not(selCP) and Ii(0));
-	Ie(1) <= (selCP and Ip(1)) or (not(selCP) and Ii(1));
-	Ie(2) <= (selCP and Ip(2)) or (not(selCP) and Ii(2));
-	Ie(3) <= (selCP and Ip(3)) or (not(selCP) and Ii(3));
-	Ie(4) <= (selCP and Ip(4)) or (not(selCP) and Ii(4));
-	Ie(5) <= (selCP and Ip(5)) or (not(selCP) and Ii(5));
-	Ie(6) <= (selCP and Ip(6)) or (not(selCP) and Ii(6));
-	Ie(7) <= (selCP and Ip(7)) or (not(selCP) and Ii(7));
-	Q0 : memoria8 port map(Ie,clk, Qs);
+	Ie(0) <= (Load and Ip(0)) or (not(Load) and Qaux(0));
+	Ie(1) <= (Load and Ip(1)) or (not(Load) and Qaux(1));
+	Ie(2) <= (Load and Ip(2)) or (not(Load) and Qaux(2));
+	Ie(3) <= (Load and Ip(3)) or (not(Load) and Qaux(3));
+	Ie(4) <= (Load and Ip(4)) or (not(Load) and Qaux(4));
+	Ie(5) <= (Load and Ip(5)) or (not(Load) and Qaux(5));
+	Ie(6) <= (Load and Ip(6)) or (not(Load) and Qaux(6));
+	Ie(7) <= (Load and Ip(7)) or (not(Load) and Qaux(7));
+	Q0 : memoria8 port map(Ie,clk, Qaux);
+	Qs <= Qaux;
 end archRegistrador8;
